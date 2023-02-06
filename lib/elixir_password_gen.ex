609abc589f28c6a234ec26d1442bb65cb572c925
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-02-05 23:31:28 -0500

    Initialized Project

---
+++ lib/elixir_password_gen.ex
@@ -0,0 +1,18 @@
+defmodule ElixirPasswordGen do
+  @moduledoc """
+  Documentation for `ElixirPasswordGen`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirPasswordGen.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_password_gen_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirPasswordGenTest do
+  use ExUnit.Case
+  doctest ElixirPasswordGen
+
+  test "greets the world" do
+    assert ElixirPasswordGen.hello() == :world
+  end
+end
